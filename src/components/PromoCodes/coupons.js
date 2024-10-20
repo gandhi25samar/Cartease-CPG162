57d8142033109d7ff008d4e8e262c206b73a3c37
gandhi25samar <gandhi25samarjeet@gmail.com>
2024-10-20 13:33:10 +0530

    Modified promo codes file

---
+++ src/components/PromoCodes/coupons.js
@@ -0,0 +1,23 @@
+const coupons = [
+  {
+    code: "GET10",
+    minValue: 1000,
+    description:
+      "Apply this promo code to get a flat 10% off on your cart value up to ₹150.",
+  },
+  {
+    code: "FLAT200",
+    minValue: 1800,
+    description:
+      "Apply this promo code to get a flat ₹200 off on your cart value.",
+  },
+  {
+    code: "PAYDAYDEAL",
+    minValue: 2500,
+    description:
+      "Apply this promo code to get a flat 20% off on your cart value up to ₹500.",
+  },
+  // Add more coupons here as needed
+];
+
+export default coupons;
