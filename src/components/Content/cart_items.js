12f0bed1194ded326d231e5de778e92d36cc0ac9
gandhi25samar <gandhi25samarjeet@gmail.com>
2024-11-21 12:54:15 +0530

    Removed id variable

--- src/components/Content/cart_items.js
+++ src/components/Content/cart_items.js
@@ -1,28 +1,28 @@
 // cart_items.js
 const cartItems = [
   {
-    id: 1,
+    // id: 1,
     productName: "Treat Chocolate Wafers",
     qty: 1,
     price: 20,
     image: "treat.jpg",
   },
   {
-    id: 2,
+    // id: 2,
     productName: "Bourbon Chocolate Biscuits",
     qty: 1,
     price: 30,
     image: "bourbon.jpg",
   },
   {
-    id: 3,
+    // id: 3,
     productName: "Tide Matic Top Load Detergent",
     qty: 10,
     price: 200,
     image: "tide.jpg",
   },
   {
-    id: 4,
+    // id: 4,
     productName: "Thums Up 1L Soft Drink",
     qty: 6,
     price: 110,
