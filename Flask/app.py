46f02deaa6687d9d5dfcd2cc74b646958612e6ac
gandhi25samar <gandhi25samarjeet@gmail.com>
2024-12-17 23:24:34 +0530

    Deleted flask deployement

--- Flask/app.py
+++ Flask/app.py
@@ -554,8 +554,8 @@ def process_image():
         "camera_blocked": camera
     })
 
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
 
 
